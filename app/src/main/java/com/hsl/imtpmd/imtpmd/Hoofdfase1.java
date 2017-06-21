27b34d62b33ddb5a852e5d78b42c962cc507e8ce
Tim Stolte <timstolte@tims-air.home>
2017-06-21 23:10:07 +0200

    uitleining + begin vullen listview h1

--- app/src/main/java/com/hsl/imtpmd/imtpmd/Hoofdfase1.java
+++ app/src/main/java/com/hsl/imtpmd/imtpmd/Hoofdfase1.java
@@ -6,11 +6,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.hsl.imtpmd.imtpmd.model.UserModel;
+import com.hsl.imtpmd.imtpmd.model.UserVerplichtvakModel;
+
+import java.util.ArrayList;
+
 
 public class Hoofdfase1 extends AppCompatActivity {
+
+    private ListView H1HoofdListview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,27 @@ public class Hoofdfase1 extends AppCompatActivity {
         setContentView(R.layout.activity_hoofdfase1);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Bundle bundle = getIntent().getExtras();
+        UserModel user = null;
+        if(bundle != null)
+            user = UserModel.getUser(getApplicationContext(), bundle.getString("user"));
+
+        H1HoofdListview = (ListView) findViewById(R.id.hoofdvakkenh1);
+
+        Log.d("Propedeuze: ", "ophalen van vakken");
+        ArrayList<UserVerplichtvakModel> verplichtvakModels = UserVerplichtvakModel.propedeuze(getApplicationContext(), user);
+
+        Log.d("propedeuze: ", "Printen van vakken");
+        for (UserVerplichtvakModel vak : verplichtvakModels) {
+            Log.d("Propedeuze", vak.getVerplichtvak().getNaam());
+        }
+
+        ListAdapter la = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                verplichtvakModels);
+
+        H1HoofdListview.setAdapter(la);
 
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
