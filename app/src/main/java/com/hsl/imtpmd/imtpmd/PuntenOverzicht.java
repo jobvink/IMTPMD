63e22a9c03b5798758c96313a3b1b88bddf4ec57
Tim Stolte <timstolte@tims-air.home>
2017-06-22 13:45:03 +0200

    data visualisatie versie 2

--- app/src/main/java/com/hsl/imtpmd/imtpmd/PuntenOverzicht.java
+++ app/src/main/java/com/hsl/imtpmd/imtpmd/PuntenOverzicht.java
@@ -1,5 +1,6 @@
 package com.hsl.imtpmd.imtpmd;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,13 +10,21 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.LargeValueFormatter;
 
 import java.util.ArrayList;
 
 public class PuntenOverzicht extends AppCompatActivity {
+
+    private BarChart chart;
+    float barWidth;
+    float barSpace;
+    float groupSpace;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,31 +33,68 @@ public class PuntenOverzicht extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        BarChart puntenOverzicht;
 
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
-        puntenOverzicht = (BarChart) findViewById(R.id.Puntenchart);
+        barWidth = 0.5f;
+        barSpace = 0f;
+        groupSpace = 0.6f;
 
-        ArrayList<BarEntry> punten = new ArrayList<>();
-        punten.add(new BarEntry(44f,0));
-        punten.add(new BarEntry(24f,0));
-        punten.add(new BarEntry(55f,0));
-        punten.add(new BarEntry(94f,0));
-        punten.add(new BarEntry(14f,0));
-        punten.add(new BarEntry(14f,0));
-        BarDataSet barDataSet = new BarDataSet(punten,"Punten");
+        chart = (BarChart)findViewById(R.id.barChart);
+        chart.setDescription(null);
+        chart.setPinchZoom(true);
+        chart.setScaleEnabled(true);
+        chart.setDrawBarShadow(false);
+        chart.setDrawGridBackground(false);
+
+        int groupCount = 3;
+
+        ArrayList xVals = new ArrayList();
+
+        xVals.add("Propedeuse");
+        xVals.add("Hoofdfase 1");
+        xVals.add("Hoofdvase 2/3");
+
+        ArrayList yVals1 = new ArrayList();
+        ArrayList yVals2 = new ArrayList();
+
+        yVals1.add(new BarEntry(1, (float) 60));
+        yVals2.add(new BarEntry(1, (float) 20));
+        yVals1.add(new BarEntry(2, (float) 60));
+        yVals2.add(new BarEntry(2, (float) 45));
+        yVals1.add(new BarEntry(3, (float) 120));
+        yVals2.add(new BarEntry(3, (float) 70));
 
 
-        BarData theData = new BarData(barDataSet);
-        puntenOverzicht.setData(theData);
 
-        puntenOverzicht.setTouchEnabled(true);
-        puntenOverzicht.setDragEnabled(true);
-        puntenOverzicht.setScaleEnabled(true);
+        BarDataSet set1, set2;
+        set1 = new BarDataSet(yVals1, "Punten totaal");
+        set1.setColor(Color.GRAY);
+        set2 = new BarDataSet(yVals2, "Punten behaald");
+        set2.setColor(Color.BLUE);
+        BarData data = new BarData(set1, set2);
+        data.setValueFormatter(new LargeValueFormatter());
+        chart.setData(data);
+        chart.getBarData().setBarWidth(barWidth);
+        chart.getXAxis().setAxisMinimum(0);
+        chart.getXAxis().setAxisMaximum(0 + chart.getBarData().getGroupWidth(groupSpace, barSpace) * groupCount);
+        chart.groupBars(0, groupSpace, barSpace);
+        chart.getData().setHighlightEnabled(false);
+        chart.invalidate();
+
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(true);
+        l.setYOffset(20f);
+        l.setXOffset(0f);
+        l.setYEntrySpace(0f);
+        l.setTextSize(8f);
+
     }
 
 
