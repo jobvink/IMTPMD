44e54516683060b6e73e4281dc14192b39c0c3fb
Tim Stolte <timstolte@tims-air.home>
2017-06-22 13:03:44 +0200

    eerste poging data visualisatie dummy data

--- app/src/main/java/com/hsl/imtpmd/imtpmd/PuntenOverzicht.java
+++ app/src/main/java/com/hsl/imtpmd/imtpmd/PuntenOverzicht.java
@@ -8,6 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+
 public class PuntenOverzicht extends AppCompatActivity {
 
     @Override
@@ -17,13 +24,33 @@ public class PuntenOverzicht extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        BarChart puntenOverzicht;
+
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
+        puntenOverzicht = (BarChart) findViewById(R.id.Puntenchart);
 
+        ArrayList<BarEntry> punten = new ArrayList<>();
+        punten.add(new BarEntry(44f,0));
+        punten.add(new BarEntry(24f,0));
+        punten.add(new BarEntry(55f,0));
+        punten.add(new BarEntry(94f,0));
+        punten.add(new BarEntry(14f,0));
+        punten.add(new BarEntry(14f,0));
+        BarDataSet barDataSet = new BarDataSet(punten,"Punten");
+
+
+        BarData theData = new BarData(barDataSet);
+        puntenOverzicht.setData(theData);
+
+        puntenOverzicht.setTouchEnabled(true);
+        puntenOverzicht.setDragEnabled(true);
+        puntenOverzicht.setScaleEnabled(true);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
