d6bc2548811585a6a47965f425f4f380e9d98c74
jobvink <jmcvink@gmail>
2017-06-23 13:51:18 +0200

    cijfer invoeren heeft informatie over het vak

--- app/src/main/java/com/hsl/imtpmd/imtpmd/CijferInvoeren.java
+++ app/src/main/java/com/hsl/imtpmd/imtpmd/CijferInvoeren.java
@@ -3,6 +3,7 @@ package com.hsl.imtpmd.imtpmd;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.hsl.imtpmd.imtpmd.model.UserModel;
 import com.hsl.imtpmd.imtpmd.model.VerplichtvakModel;
@@ -14,21 +15,29 @@ public class CijferInvoeren extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cijfer_invoeren);
 
+        TextView invoer_code = (TextView) findViewById(R.id.invoer_code);
+        TextView invoer_naam = (TextView) findViewById(R.id.invoer_naam);
+
+        Bundle bundle = getIntent().getExtras();
+        String code = null;
+        String naam = null;
+        int cijfer;
+        Boolean behaald;
+        if(bundle != null){
+            code    = bundle.getString("code");
+            naam    = bundle.getString("naam");
+            cijfer  = bundle.getInt("cijfer");
+            behaald = bundle.getBoolean("behaald");
+        }
+        if (code!=null) {
+            invoer_code.setText(code);
+            invoer_naam.setText(naam);
+        }
+
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-            Bundle bundle = getIntent().getExtras();
-            String code;
-            String naam;
-            int cijfer;
-            Boolean behaald;
-            if(bundle != null){
-                code    = bundle.getString("code");
-                naam    = bundle.getString("naam");
-                cijfer  = bundle.getInt("cijfer");
-                behaald = bundle.getBoolean("behaald");
-            }
 
 
         }
